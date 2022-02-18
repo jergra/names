4fc697e805a77427df9dc34f3598b90568c27e95
jergra <jergra43@gmail.com>
2022-02-18 12:26:38 -0800

    increase new game wait

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -56,7 +56,7 @@ export const getWordOfDay = () => {
   const epochMs = new Date('January 1, 2022 00:00:00').valueOf()
   const now = Date.now()
   //const msInDay = 86400000
-  const msInDay = 140000
+  const msInDay = 200000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
 
